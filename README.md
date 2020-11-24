83a2e0f29df0d78fa8db815ea4e2bc951da791ab
Jonas Keßler <jonas.kessler@aclue.de>
2020-11-24 14:26:11 +0100

    Initial project with model classes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # demo-dependency-injection-01
+Demo Projekt zu Dependency Injection in Spring Boot mit ausschließlich zentraler Bean-Konfiguration.
---
+++ src/main/java/de/aclue/demodependencyinjection01/DemoDependencyInjection01Application.java
@@ -0,0 +1,13 @@
+package de.aclue.demodependencyinjection01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDependencyInjection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDependencyInjection01Application.class, args);
+	}
+
+}
